cf7e349c80c54e5365c4f6af17c158c303c1ee28
Yella Yohan Reddy <66876592+YohanReddy@users.noreply.github.com>
2024-10-21 13:13:53 +0000

    update api

--- app/api/generate-image/route.ts
+++ app/api/generate-image/route.ts
@@ -4,6 +4,8 @@ import OpenAI from 'openai';
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+export const dynamic = 'force-dynamic';
+
 
 export async function POST(request: Request) {
   try {
