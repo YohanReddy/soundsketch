d546f7805edfaf4637678524c6be4bfb56b36c18
Yella Yohan Reddy <66876592+YohanReddy@users.noreply.github.com>
2024-10-21 21:36:15 +0530

    Fix: Next Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "oaidalleapiprodscus.blob.core.windows.net",
+      "soundsketch.vercel.app",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oaidalleapiprodscus.blob.core.windows.net",
+      },
+      {
+        protocol: "https",
+        hostname: "soundsketch.vercel.app",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
